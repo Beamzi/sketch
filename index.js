0b0b1226cc591975819c2d4db6d5e54fdd683339
james <159420270+Beamzi@users.noreply.github.com>
2024-05-25 11:34:32 +1000

    update includes opacity effect on multiple hovers and random colors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const gridContainer = document.querySelector('.gridContainer');
 
 const gridDivs = () => {
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < 4096; i++) {
     let newDivs = document.createElement('div');
     newDivs.classList.add('allDivs');
     gridContainer.append(newDivs);
@@ -17,15 +17,30 @@ gridDivs()
 
 const allDivsref = document.querySelectorAll('.allDivs');
 
+
 const drawing = () => {
   const allDivsref = document.querySelectorAll('.allDivs');
+
   allDivsref.forEach((Mouse) => {
+    let hoverCount = 0;
+    let opacity = 0.1;
     Mouse.addEventListener('mouseover', (e) => {
-      e.target.style['background-color'] = 'black';
+      let x = Math.floor(Math.random() * 255);
+      let y = Math.floor(Math.random() * 255);
+      let z = Math.floor(Math.random() * 255);
+      let random = `rgb(${x}, ${y}, ${z})`;
+     // e.target.style['background-color'] = 'black';
+      e.target.style['background-color'] = random;
+      hoverCount++;
+      if (hoverCount <= 10)
+        //e.target.style.opacity = `${hoverCount * 10}%`
+        e.target.style.opacity = hoverCount * opacity;
     });
   });
 };
+
 drawing();
+
 
 
 
@@ -40,8 +55,8 @@ newSketch = () => {
       div.remove();
     });
 
-    userInput = parseInt(prompt('Type in the number of pixels you want your canvas'));
-    for (let i = 0; i < userInput; i++) {
+    userInput = parseInt(prompt('Type in the number of pixels you want on each side'));
+    for (let i = 0; i < userInput * userInput; i++) {
 
       let newSketchDivs = document.createElement('div');
       newSketchDivs.classList.add('allDivs');
